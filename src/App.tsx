b18427b3fcbc2ed11eae7faf592956acccfb3555
Raj <rajdave7319@gmail.com>
2025-09-19 13:50:47 -0500

    Make Initial Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,39 +7,21 @@ const schedule = {
       "number": "101",
       "meets" : "MWF 11:00-11:50",
       "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
     }
+    
   }
 };
 
 
 const App = () => {
-  const today = new Date();
-  const day = today.toLocaleString([], {weekday: 'long'});
-  const date = today.toLocaleDateString([], {dateStyle: 'long'})
 
   return (
     <>
-      <h1>Sample React code</h1>
+      <h1>{schedule.title}</h1>
       
-      <p>This page was last loaded on {day}, {date}.</p>
+      <p></p>
+      <h2>{schedule.courses.F101.term} {schedule.courses.F101.title} {schedule.courses.F101.meets} {schedule.courses.F101.number}</h2>
+
     </>
   );
 };
